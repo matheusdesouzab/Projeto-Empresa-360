2818f8c50ecfa5987d36ac7fe59580e5f1dfdf5c
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-19 10:32:49 -0300

    Utilizando o route-link para navegar entre as rotas

--- src/views/HomeComponent.vue
+++ src/views/HomeComponent.vue
@@ -1,3 +1,4 @@
 <template>
     <h3>Componente home</h3>
+    <route-link to="/login">Navegar para login</route-link>
 </template>
--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -1,3 +1,4 @@
 <template>
     <h3>Componente Login</h3>
+    <route-link to="/home">Navegar para home</route-link>
 </template>
