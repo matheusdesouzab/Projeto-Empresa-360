87f7fd6ff0793de2177f047b05e4e81e02d8a6c9
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-19 19:07:45 -0300

    Implementação da interface do componente home

--- src/views/HomeComponent.vue
+++ src/views/HomeComponent.vue
@@ -1,4 +1,54 @@
 <template>
-    <h3>Componente home</h3>
-    <router-link to="/">Sair</router-link>
+    <div>
+        <nav class="navbar navbar-light bg-primary menu-superior border-bottom border-warning border-5">
+            <div class="container">
+                <a class="navbar-brand fw-bold text-info" href="#">Empresa <span class="text-warning">360</span></a>
+                <div class="navbar-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-white fw-bold" href="#">Sair</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="d-flex">
+            <div class="list-group menu-esquerda">
+                <a class="list-group-item list-group-item-action" href="#"><i class="bi bi-speedometer2"></i> Dashboard</a>
+            </div>
+
+            <div class="w-100">
+                <nav aria-label="breadcrumb" class="pt-1 ps-3">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </nav>
+
+                <div class="container-fluid">
+                    <h1 class="mt-4">Visões relacionadas ao menu</h1>
+                    <p>Aqui podemos apresentar as views de acordo com as opções do menu.</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
+
+
+<style scoped>
+.menu-superior {
+    border-bottom: solid 1px #ccc;
+}
+
+.menu-esquerda {
+    border-right: solid 1px #ccc;
+    width: 250px;
+}
+
+.list-group-item {
+    border-top: none;
+    border-right: none;
+}
+
+</style>
