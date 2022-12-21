630b88e201cddc0e49bbad29012968a790bff888
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-20 22:54:27 -0300

    Implementação da interface do component 'Contratos' e atualização dos dados do arquivo json-server, agora ele possui os dados de contratos

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,6 +1,38 @@
 <template>
-    <h5>Contratos</h5>
+    <div>
+        <h5>Contratos</h5>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Lead</th>
+                    <th scope="col">Serviço</th>
+                    <th scope="col">Data início</th>
+                    <th scope="col">Data final</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="d in dados" :key="d.id">
+                    <td>{{ d.id }}</td>
+                    <td>{{ d.lead}}</td>
+                    <td>{{ d.servico }}</td>
+                    <td>{{ d.data_inicio }}</td>
+                    <td>{{ d.data_fim }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+
+import ApiMixin from '@/mixins/ApiMixin'
+
+export default {
+    name: 'Contratos',
+    mixins: [ApiMixin],
+    created() {
+        this.getDados('http://localhost:3000/contratos')
+    }
+}
 </script>
