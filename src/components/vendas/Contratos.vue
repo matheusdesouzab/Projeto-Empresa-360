ff0931c11b359663649ee8cd2262aad57213715f
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-21 00:19:39 -0300

    Criação dos botões de pesquisas e criação do método 'beforeRouteUpdate' para observar as mudanças na url

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h5>Contratos</h5>
+
+        <router-link class="btn btn-success" :to="{ name: 'contratos', query: { leadId_like: 1 } }">LeadId =
+            1</router-link>
+        <router-link class="btn btn-primary ms-3" :to="{ name: 'contratos', query: { leadId_like: 1, servicoId_like: 2 } }">ServicoId = 2 e LeadId 1</router-link>
+
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -15,7 +20,7 @@
                 <tr v-for="d in dados" :key="d.id">
                     <td>{{ d.id }}</td>
                     <td>{{ d.lead.nome }}</td>
-                    <td>{{ d.servico.servico}}</td>
+                    <td>{{ d.servico.servico }}</td>
                     <td>{{ d.data_inicio }}</td>
                     <td>{{ d.data_fim }}</td>
                 </tr>
@@ -31,6 +36,16 @@ import ApiMixin from '@/mixins/ApiMixin'
 export default {
     name: 'Contratos',
     mixins: [ApiMixin],
+    data: () => ({
+        parametrosDoRelacionamento: '_expand=lead&_expand=servico'
+    }),
+    beforeRouteUpdate(to, from, next) {
+        const queryParams = new URLSearchParams(to.query).toString()
+        const url = `http://localhost:3000/contratos?${this.parametrosDoRelacionamento}&${queryParams}`
+        this.getDados(url)
+
+        next()
+    },
     created() {
         this.getDados('http://localhost:3000/contratos?_expand=lead&_expand=servico')
     }
